c0d97bc3f713803d09b016b0e2ff1aa1466bf2d8
StasiuTomkowiak <tomkowiakstas@gmail.com>
2025-04-27 13:41:56 +0200

    js lint optimazition

--- portfolio/scripts/syntax-highlight.js
+++ portfolio/scripts/syntax-highlight.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    
-    const codeElements = document.querySelectorAll('code.code');
-    
+document.addEventListener("DOMContentLoaded", function() {
+
+    const codeElements = document.querySelectorAll("code.code");
+
     codeElements.forEach(codeBlock => {
-    
-        codeBlock.classList.add('language-python');
-   
+
+        codeBlock.classList.add("language-python");
+
         const content = codeBlock.textContent;
-   
+
         codeBlock.textContent = content;
-        
+
         Prism.highlightElement(codeBlock);
     });
 });
